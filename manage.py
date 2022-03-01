39922bd37be24e4443288caf8b7ca775792835f1
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-01 20:37:12 +0300

    deploying to heroku

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 from app import create_app
-from flask_script import Manager, Server
+# from flask_script import Manager, Server
 
 
 #Creating app instance
 app = create_app('development')
 
-manager = Manager(app)
-manager.add_command('server', Server)
+# manager = Manager(app)
+# manager.add_command('server', Server)
 
 if __name__ == '__main__':
-    manager.run()                                                                                                                                                   
+    app.run()                                                                                                                                                   
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export NEWS_API_KEY="c1105b28097b432b805ecbc526a1acb1"
 export  SECRET_KEY="vyonna-api"
-python3.8 manage.py server
+python3.8 manage.py 
